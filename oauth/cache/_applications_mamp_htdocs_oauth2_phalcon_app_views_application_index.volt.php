824c75fdb5d67bcc204b128fabd34107bc8d85c3
KaijiS <31200760+KaijiS@users.noreply.github.com>
2019-01-15 17:23:40 +0900

    edit_vendor_dir

--- oauth/cache/_applications_mamp_htdocs_oauth2_phalcon_app_views_application_index.volt.php
+++
@@ -1,9 +0,0 @@
-<div class="page-header">
-    <h1>Congratulations!</h1>
-</div>
-
-<p>loginしました</p>
-
-<?= $user['login'] ?> <br>
-<?= $user['bio'] ?> <br>
-<?= $this->tag->linkto(['session/logout', 'ログアウト', 'local' => false, 'class' => 'btn', 'title' => 'logout']) ?>
--- oauth/cache/_applications_mamp_htdocs_oauth2_phalcon_app_views_session_error.volt.php
+++
@@ -1,7 +0,0 @@
-<div class="page-header">
-    <h1>Session Error</h1>
-    <h3>ログインされていません</h3>
-
-    <?= $this->tag->linkto(['session', 'TOPへ', 'local' => true, 'class' => 'btn', 'title' => 'error2top']) ?>
-    <?= $this->tag->linkto(['session/login', 'ログインする', 'local' => true, 'class' => 'btn', 'title' => 'error2login']) ?>
-</div>
